d7a14ddbc55fcadb1dabf9c0e62f8aade96c9d5e
AD-AFGOAL <95408504+AD-AFGOAL@users.noreply.github.com>
2022-08-13 22:48:17 +0200

    amelioration

--- app.py
+++ app.py
@@ -13,73 +13,16 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from models import db, app, Venue, Artist, Show
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
-moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
+
 
 # TODO: connect to a local postgresql database
 migration = Migrate(app, db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
 
-class Venue(db.Model):
-    __tablename__ = 'venues'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    image_link = db.Column(db.String(500), nullable=False)
-    facebook_link = db.Column(db.String(120), nullable=False)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-def __repr__(self):
-     return f'<venues ID: {self.id}, name: {self.name}, city: {self.city}, state: {self.state}, address: {self.address}, phone: {self.phone}, image_link: {self.image_link}, facebook_link: {self.facebook_link}>'
-
-class Artist(db.Model):
-    __tablename__ = 'artists'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    state = db.Column(db.String(120), nullable=False)
-    phone = db.Column(db.String(120), nullable=False)
-    genres = db.Column(db.String(120), nullable=False)
-    image_link = db.Column(db.String(500), nullable=False)
-    facebook_link = db.Column(db.String(120), nullable=False)
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-def __repr__(self):
-     return f'<artists ID: {self.id}, name: {self.name}, city: {self.city}, state: {self.state}, genres: {self.genres}, phone: {self.phone}, image_link: {self.image_link}, facebook_link: {self.facebook_link}>'
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
-class Shows(db.Model):
-    __tablename__ ='shows'
-
-    id = db.Column(db.Integer, primary_key=True)
-
-#----------------------------------------------------------------------------#
-# Filters.
-#----------------------------------------------------------------------------#
-
-def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
-  if format == 'full':
-      format="EEEE MMMM, d, y 'at' h:mma"
-  elif format == 'medium':
-      format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format, locale='en')
-
-app.jinja_env.filters['datetime'] = format_datetime
 
 #----------------------------------------------------------------------------#
 # Controllers.
